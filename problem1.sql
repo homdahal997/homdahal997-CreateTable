b57d69729842077060064843ffaa65ef4069defa
homdahal997 <homdahal997@gmail.com>
2023-03-07 11:03:26 +0000

    Create table lab

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE song (
+  title VARCHAR(100),
+  artist VARCHAR(100)
+);
+INSERT INTO song (title, artist) VALUES ('Let it be', 'Beatles');
+INSERT INTO song (title, artist) VALUES ('Hotel California', 'Eagles');
+INSERT INTO song (title, artist) VALUES ('Kashmir', 'Led Zeppelin');
